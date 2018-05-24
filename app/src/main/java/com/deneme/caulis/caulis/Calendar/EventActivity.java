f1ad95b9a5157ce983a718e67b34c0febe37aee1
Victor <victor.siguier@gmail.com>
2018-05-24 11:32:44 +0200

    Calendar event added

---
+++ app/src/main/java/com/deneme/caulis/caulis/Calendar/EventActivity.java
@@ -0,0 +1,18 @@
+package com.deneme.caulis.caulis.Calendar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.deneme.caulis.caulis.R;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+    }
+    
+    }
