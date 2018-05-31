1b4df6ece9c7b27027ad717190bc6789662d1f9d
ivrindi <emrey4260@gmail.com>
2018-05-31 12:37:01 +0300

    Class containers added

---
+++ app/src/main/java/com/deneme/caulis/caulis/classes/CaulisEvent.java
@@ -0,0 +1,25 @@
+
+/***************************************************************************************************
+ * A container for events
+ **************************************************************************************************/
+
+package com.deneme.caulis.caulis.classes;
+
+
+import java.sql.Time;
+import java.util.Date;
+
+public class CaulisEvent {
+    private String name;
+    private Date startDate;
+    private Date endDate;
+    private Time startTime;
+    private Time endTime;
+    private String location;
+    private String description;
+    private int numberOfPeopleAllowed;/*Not necessary*/
+    private int eventID;
+    private int authorID;
+    /*private arraylist<> users;*/
+
+}
---
+++ app/src/main/java/com/deneme/caulis/caulis/classes/CaulisGroup.java
@@ -0,0 +1,16 @@
+
+/***************************************************************************************************
+ * A container for events
+ **************************************************************************************************/
+
+
+package com.deneme.caulis.caulis.classes;
+
+public class CaulisGroup {
+    private String name;
+    private String description;
+    private int groupID;
+    private int authorID;
+    private int numberOfPeopleAllowed;
+    /*private arraylist<> users;*/
+}
