4417b60260ced3e2d2615a3447e357d94f2d383e
ivrindi <emrey4260@gmail.com>
2018-06-14 13:44:27 +0300

    Some necessary changes...

--- app/src/main/java/com/deneme/caulis/caulis/classes/CaulisEvent.java
+++ app/src/main/java/com/deneme/caulis/caulis/classes/CaulisEvent.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CaulisEvent {
+public class CaulisEvent implements CaulisClasses{
 
     private Map<String, List<Events>> allEvents = new HashMap<>();
     private Calendar eventsCalendar;
